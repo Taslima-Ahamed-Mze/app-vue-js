ed981aadb9fefaeb6f2f3462a6d87f601f197920
Julien Martini <jmartini@linguaphone.fr>
2022-12-19 15:12:00 +0100

    Mon projet vuejs

---
+++ src/components/HomeLink.vue
@@ -0,0 +1,15 @@
+<template>
+  <a href="/">Accueil</a>
+</template>
+
+<script>
+export default {
+  name: "HomeLink"
+}
+</script>
+
+<style scoped>
+  a{
+    text-decoration: none;
+  }
+</style>
---
+++ src/components/MenuItem.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  name: "MenuItem",
+  props: ["addToShoppingCart", "image", "inStock", "name", "quantity"],
+  data() {
+    return {
+      qty: this.quantity
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="menu-item">
+    <img class="menu-item__image" :src="image.source" :alt="image.alt" />
+    <div>
+      <h3>{{ name }}</h3>
+      <p v-if="inStock">En stock</p>
+      <p v-else>En rupture de stock</p>
+      <div>
+        <label for="add-item-quantity">Quantité : {{ qty}}</label>
+        <input v-model.number="qty" id="add-item-quantity" type="number" />
+        <button @click="addToShoppingCart(qty)">
+          Ajouter au panier
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style></style>
